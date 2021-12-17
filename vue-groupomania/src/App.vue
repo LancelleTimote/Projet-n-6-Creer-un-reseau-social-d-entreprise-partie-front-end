ec02ba0a19b556fcd67cbdffe9c4b3fc8224637b
Timoté LANCELLE <timote.lancelle@outlook.fr>
2021-12-17 14:12:43 +0100

    Ajout de la page d'accueil à vue, suppression des codes inutiles vue, importation des images à vue

--- vue-groupomania/src/App.vue
+++ vue-groupomania/src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    //base.scss
+    body {
+		margin:0;
+        font-family: Arial, Verdana, 'Times New Roman', Tahoma, sans-serif;
+        main {
+            background-image: url(assets/building_1.jpg);
+            background-size: cover;
+            flex-grow:1;
+        }
+        p, a {
+            color: rgba(255, 255, 255, 1);
+        }
+        a {
+            text-decoration: none;
+        }
+        a:hover {
+            color: rgba(39, 72, 128, 1);
+        }
+        p {
+            margin: 0;
+        }
+        ul {
+            list-style: none;
+            padding: 0;
+        }
+        @media (min-width: 1440px) {
+            .container {
+                width: 1440px;
+            }
+        }
     }
-  }
-}
 </style>
--- vue-groupomania/src/views/Home.vue
+++ vue-groupomania/src/views/Home.vue
@@ -1,18 +1,122 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <header>
+            <div class="container">
+                <a href="index.html" class="animation__link"><img src="../assets/logo_white_nav.png" alt="Petit logo de l'entreprise Groupomania" title="C'est un petit logo de l'entreprise Groupomania." /></a>
+                <ul>
+                    <li>
+                        <a href="connexion.html" class="animation__link">Connexion</a>
+                    </li>
+                    <li>
+                        <a href="inscription.html" class="animation__link">Inscription</a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+        <main class="main__index">
+            <div class="container">
+                <section>
+                    <p><img src="../assets/logo_white_home.png" alt="Logo de l'entreprise Groupomania" title="C'est un logo de l'entreprise Groupomania."/></p>
+                    <p>Bienvenue sur votre réseau social d'entreprise</p>
+                    <ul>
+                        <li>
+                            <a href="connexion.html" class="animation__link">Connexion</a>
+                        </li>
+                        <li>
+                            <a href="inscription.html" class="animation__link">Inscription</a>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+        </main>
+        <footer>
+            <div class="container">
+                <ul>
+                    <li>
+                        <a href="a_propos.html" class="animation__link">À propos</a>
+                    </li>
+                    <li>
+                        <a href="mentions_legales.html" class="animation__link">Mentions légales</a>
+                    </li>
+                    <li>
+                        <a href="support.html" class="animation__link">Support</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style scoped lang="scss">
+    //home.scss
+    .home {
+        display: flex;
+        flex-direction: column;
+        min-height:100vh;
+    }
+    //header.scss
+    header {
+        border-bottom: 5px solid rgba(39, 72, 128, 1);
+        height: 75px;
+        .container {
+            display: flex;
+            justify-content: space-between;
+            ul {
+                display: flex;
+                li {
+                    margin: 0 35px;
+                }
+            }
+        }
+    }
+    //footer.scss
+    footer {
+        border-top: 5px solid rgba(39, 72, 128, 1);
+        position: sticky;
+        bottom: 0;
+        height: auto;
+        .container {
+            display: flex;
+            justify-content: center;
+            ul {
+                display: flex;
+                li {
+                    margin: 10px 35px;
+                }
+            }
+        }
+    }
+    //header_footer.scss
+    header, footer {
+        background-color: rgba(160, 160, 160, 1);
+        font-size: 20px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        @media (max-width: 1440px) {
+            .container {
+                width: 100%;
+            }
+        }
+    }
+    //index.scss
+    .main__index {
+        font-size: 30px;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        li {
+            margin-bottom: 20px;
+        }
+    }
+</style>
